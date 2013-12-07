5705385fa01c8a6db2f78e747c46ed791ecda160
GoGoYellowScreen <kheffern@gmail.com>
2013-12-07 15:54:06 -0800

    Create WrapListModel.cs

---
+++ WrapListModel.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NameThatWrap.Models
+{
+    public class WrapListModel
+    {
+            public List<WrapModel> WrapList { get; set; }
+    }
+}
