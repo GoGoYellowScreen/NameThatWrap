4256705ea18ff4f9881ca4ebc629ac2979edc1cf
GoGoYellowScreen <kheffern@gmail.com>
2013-12-07 15:54:48 -0800

    Create WrapModel.cs

---
+++ WrapModel.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NameThatWrap.Models
+{
+    public class WrapModel
+    {
+        public string ImgName { get; set; }
+        public string Colorway { get; set; }
+        public int WrapID { get; set; }
+    }
+}
